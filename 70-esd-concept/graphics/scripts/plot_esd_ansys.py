2c307b10e31462de62d4dff3a9259f210525c238
Sean Leavey <sean.leavey@gmail.com>
2016-01-28 23:22:11 +0000

    Updates to ESD chapter

--- 70-esd-concept/graphics/scripts/plot_esd_ansys.py
+++ 70-esd-concept/graphics/scripts/plot_esd_ansys.py
@@ -14,7 +14,7 @@ data_path = sys.argv[2]
 data = np.genfromtxt(data_path, delimiter=',', skip_header=1)
 
 # fit
-gradient, intercept, _, _, _ = stats.linregress(data[:, 0], data[:, 3])
+gradient, intercept, _, _, _ = stats.linregress(data[:, 0], data[:, 6])
 
 # fit x-axis
 fitx = xrange(600, 800, 10)
@@ -27,21 +27,24 @@ ax1 = plt.gca()
 colours = lf.Colours()
 
 # plot
-ax1.plot(data[:, 0], data[:, 1], 'bx', ms=lf.MS_A, mew=lf.MEW_A, color=colours.next(), alpha=lf.ALPHA_LINE_A)
-ax1.plot(data[:, 0], data[:, 2], 'r+', ms=lf.MS_A, mew=lf.MEW_A, color=colours.next(), alpha=lf.ALPHA_LINE_A)
-ax1.plot(data[:, 0], data[:, 3], 'gx', ms=lf.MS_A, mew=lf.MEW_A, color=colours.next(), alpha=lf.ALPHA_LINE_A)
+ax1.plot(data[:, 0], data[:, 4], 'x', ms=lf.MS_A, mew=lf.MEW_A, color=colours.next(), alpha=lf.ALPHA_LINE_A)
+ax1.plot(data[:, 0], data[:, 5], '+', ms=lf.MS_A, mew=lf.MEW_A, color=colours.next(), alpha=lf.ALPHA_LINE_A)
+ax1.plot(data[:, 0], data[:, 6], 'x', ms=lf.MS_A, mew=lf.MEW_A, color=colours.next(), alpha=lf.ALPHA_LINE_A)
 ax1.plot(fitx, fitx * gradient + intercept, '--', color=colours.current(), alpha=0.8)
+
+# enforce log tick labels
+ax1.yaxis.get_major_formatter().set_powerlimits((0, 0))
 
 ax1.set_xlabel('Voltage [V]')
 ax1.set_ylabel('Force [N]')
 
-ax1.legend(['Force in x-direction', 'Force in y-direction', 'Force in z-direction', 'Fit for force in z-direction'], borderaxespad=lf.BORDER_AXIS_PAD)
+ax1.legend(['Force in x-direction', 'Force in y-direction', 'Force in z-direction', 'Fit for force in z-direction\n(gradient = {:.2f} nN/V)'.format(gradient * 1e9)], borderaxespad=lf.BORDER_AXIS_PAD)
 
 ax1.set_xticks(xrange(650, 751, 10))
 
 ax1.grid(True)
 
 ax1.set_xlim([640, 760])
-ax1.set_ylim([-2.5e-7, 0.1e-7])
+ax1.set_ylim([-1.6e-6, 0.5e-7])
 
 plt.savefig(save_path)
