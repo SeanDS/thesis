80543a65fcc4a808ca686b557785a53f07f35525
Sean Leavey <sean.leavey@gmail.com>
2016-06-13 19:14:18 +0100

    Fixed path issue in scripts

--- scripts/word-count.sh
+++ scripts/word-count.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cd ..
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR/..
+
 texcount -inc -total thesis.tex
--- scripts/word-frequency.sh
+++ scripts/word-frequency.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cd ..
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR/..
+
 texcount -inc -freq thesis.tex
