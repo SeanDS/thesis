Fixed units on Paschen curve

graphics/sources/python/60-esd-paschen.py
@@ -19,12 +19,18 @@ def paschen(A, B, p, d, gamma):
 # (source taken from Wikipedia: https://en.wikipedia.org/wiki/File:Paschen_curves.svg)
 A = 11.8
 B = 325
-p = np.logspace(-2, 7, 100000)
+p = np.logspace(-2, 7, 100000) # in pascals
 d = np.logspace(-1, -3, 3)
 gamma_se = 0.01
 
 # generate data
 V = [paschen(A, B, p, this_d, gamma_se) for this_d in d]
+
+# convert p from pascals to mbar
+# 1 Pa = 1e-5 bar
+# 1 bar = 1e3 mbar
+# so 1 Pa = 1e-2 mbar
+p *= 1e-2
 
 fig = plt.figure(figsize=lf.FIG_SIZE_A)
 
@@ -35,13 +41,13 @@ colours = lf.Colours()
 
 # plot, converting to mbar
 for i in range(len(d)):
-    ax1.loglog(p * 1e2, V[i], color=colours.next(), alpha=lf.ALPHA_LINE_A)
+    ax1.loglog(p, V[i], color=colours.next(), alpha=lf.ALPHA_LINE_A)
 
 # horizontal line representing maximum voltage
-ax1.hlines(750, (p*1e2).min(), (p*1e2).max(), colors=colours.next(), linestyles='dashed', zorder=2)
+ax1.hlines(750, p.min() * 1e-2, p.max() * 1e2, colors=colours.next(), linestyles='dashed', zorder=2)
 
 # vertical line representing atmospheric pressure
-ax1.vlines(1e8, 1e1, 1e6, colors=colours.next(), linestyles='dashed', zorder=2)
+ax1.vlines(1e3, 1e1, 1e6, colors=colours.next(), linestyles='dashed', zorder=2)
 
 ax1.set_xlabel('Pressure [mbar]')
 ax1.set_ylabel('Breakdown Voltage [V]')
@@ -60,7 +66,7 @@ ax1.legend(legend_strings, loc='upper left')
 
 ax1.grid(True)
 
-ax1.set_xlim([1e2, 1e9])
+ax1.set_xlim([1e-5, 1e4])
 ax1.set_ylim([1e2, 1e5])
 
 plt.tight_layout()