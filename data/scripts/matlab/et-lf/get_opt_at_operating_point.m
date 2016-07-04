f2ccae33df32fa47713a2b797e790506b531fd81
Sean Leavey <s.leavey.1@research.gla.ac.uk>
2016-07-04 12:23:13 +0100

    Set readouts and demodulation phases for all degrees of freedom

--- data/scripts/matlab/et-lf/get_opt_at_operating_point.m
+++ data/scripts/matlab/et-lf/get_opt_at_operating_point.m
@@ -6,24 +6,34 @@ function opt = get_opt_at_operating_point(varargin)
     m_sense = zeros(1, opt.Nprobe);
     m_sense(opt.getProbeNum('POP_11_I')) = 1; % PRCL
     m_drive = zeros(opt.Ndrive, 1);
-    m_drive(opt.getDriveNum('PR')) = -1;
+    m_drive(opt.getDriveNum('BS')) = 0.15;
+    m_drive(opt.getDriveNum('SR')) = 0.5;
+    m_drive(opt.getDriveNum('PR')) = -0.5;
     
     % set the operating point
-    %[opt, pos] = opt.setOperatingPoint(m_drive, m_sense);
-    %pos
+    %[opt, ~] = opt.setOperatingPoint(m_drive, m_sense);
     
-    m_sense = zeros(1, opt.Nprobe);
-    m_sense(opt.getProbeNum('POP_11_I')) = 1; % PRCL
-    m_sense(opt.getProbeNum('REFL_57_Q')) = 1; % MICH
-    m_drive = zeros(opt.Ndrive, 1);
-    m_drive(opt.getDriveNum('BS')) = -1;
-    m_drive(opt.getDriveNum('PR')) = 1;
-    m_drive(opt.getDriveNum('SR')) = -1;
+    %% Set demodulation phases
     
-    % set the operating point
-    [opt, pos] = opt.setOperatingPoint(m_drive, m_sense);
-    pos
+    % phases read from combined I and Q sensing matrix
+    demod_carm = -71.71;
+    demod_mich = 80.5; % offset appears for slightly different phase
+    demod_prcl = -14.33;
+    demod_srcl = -168.18;
     
-    % set Schnupp and DARM again
-    %opt.setPosOffset('IX',
+    % CARM
+    opt.setProbePhase('REFL_11_I', demod_carm);
+    opt.setProbePhase('REFL_11_Q', demod_carm + 90);
+    
+    % MICH
+    opt.setProbePhase('POP_57_I', demod_mich - 90);
+    opt.setProbePhase('POP_57_Q', demod_mich);
+    
+    % PRCL
+    opt.setProbePhase('POP_11_I', demod_prcl);
+    opt.setProbePhase('POP_11_Q', demod_prcl + 90);
+    
+    % SRCL
+    opt.setProbePhase('REFL_68_I', demod_srcl - 90);
+    opt.setProbePhase('REFL_68_Q', demod_srcl);
 end
