d9c95fe5ae4cd9b00a1ebdf8063b34aa912b3976
Sean Leavey <s.leavey.1@research.gla.ac.uk>
2016-09-02 19:33:17 +0100

    Some fixes to the appendix, work in progress

--- graphics/sources/python/AA-sideband-structure.py
+++ graphics/sources/python/AA-sideband-structure.py
@@ -1,6 +1,8 @@
+from __future__ import division
+
 import sys
 import numpy as np
-import scipy.stats as stats
+import scipy.signal
 import matplotlib.pyplot as plt
 import lookfeel as lf
 
@@ -12,15 +14,15 @@ fig = plt.figure(figsize=lf.FIG_SIZE_A)
 ax = plt.gca()
 
 # turn off y-axis graphics
-ax.tick_params(
-    axis='both',
-    which='both',
-    left='off',
-    labelleft='off',
-    right='off',
-    labelright='off',
-    top='off',
-    labeltop='off')
+#ax.tick_params(
+#    axis='both',
+#    which='both',
+#    left='off',
+#    labelleft='off',
+#    right='off',
+#    labelright='off',
+#    top='off',
+#    labeltop='off')
 
 # turn off all spines except lower x
 ax.spines['left'].set_visible(False)
@@ -37,6 +39,38 @@ c3 = colours.next() # amplitude modulation sidebands
 
 # arrow arguments
 args = {'width': lf.ARROW_WIDTH, 'head_width': lf.ARROW_HEAD_WIDTH, 'head_length': lf.ARROW_HEAD_LENGTH, 'overhang': lf.ARROW_OVERHANG, 'length_includes_head': True}
+
+# amplitude modulation
+def am(E_0, omega_0, t, m, omega):
+  return [E_0 * np.exp(1j * omega_0 * t) * (m / 2) * np.exp(1j * omega * t), E_0 * np.exp(1j * omega_0 * t), E_0 * np.exp(1j * omega_0 * t) * (m / 2) * np.exp(-1j * omega * t)]
+
+# phase modulation
+def pm(E_0, omega_0, t, m, omega, n):
+  
+  # ith Bessel coefficient
+  def ji(E_0, omega_0, t, m, omega, i):
+    return 1j ** i * np.i0(m) * np.exp(1j * i * omega * t)
+  
+  # return list of coefficients up to nth, multiplied by carrier
+  return E_0 * np.exp(1j * omega_0 * t) * [ji(E_0, omega_0, t, m, omega, ii) for ii in xrange(i)]
+
+E_0 = 1
+c_0 = 3e8
+lambda_0 = 1064e-9
+omega_0 = 2 * np.pi * c_0 / lambda_0
+t = np.linspace(0, 1e-5, 100000)
+m = 0.1
+omega = 10e6
+
+am_fields = am(E_0, omega_0, t, m, omega)
+#[f, p_am] = scipy.signal.welch(sum(am_fields), fs=1/(t[1]-t[0]), window='hann', nperseg=10000)
+
+ax.plot(t, np.abs(am_fields[0]))
+#ax.semilogy(f, p_am)
+#ax.plot(t, am_fields[1])
+#ax.plot(t, am_fields[2], '--')
+#ax.set_xlim([0, 3e-14])
+plt.show()
 
 ## plot carrier
 p1 = ax.arrow(0, 0, 0, 15, color=c1, **args)
@@ -77,4 +111,4 @@ ax.grid(True)
 ax.set_xlim([-1.3, 1.3])
 ax.set_ylim([0, 16])
 
-plt.savefig(save_path)
+#plt.savefig(save_path)
