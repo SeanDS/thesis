cfaed62c5429040a59ad808a814b0272ab640288
Sean Leavey <sean.leavey@gmail.com>
2016-09-03 10:38:31 +0100

    Fixed plot of modulation and description, removed some duplicate text

--- graphics/sources/python/AA-sideband-structure.py
+++ graphics/sources/python/AA-sideband-structure.py
@@ -2,7 +2,8 @@ from __future__ import division
 
 import sys
 import numpy as np
-import scipy.signal
+import numpy.fft as fft
+import scipy.special as special
 import matplotlib.pyplot as plt
 import lookfeel as lf
 
@@ -11,23 +12,30 @@ save_path = sys.argv[1]
 
 # figure and axis
 fig = plt.figure(figsize=lf.FIG_SIZE_A)
-ax = plt.gca()
+ax1 = plt.subplot(211)
+ax2 = plt.subplot(212)
 
 # turn off y-axis graphics
-#ax.tick_params(
-#    axis='both',
-#    which='both',
-#    left='off',
-#    labelleft='off',
-#    right='off',
-#    labelright='off',
-#    top='off',
-#    labeltop='off')
+tick_params = {
+    'axis': 'both',
+    'which': 'both',
+    'left': 'off',
+    'labelleft': 'off',
+    'right': 'off',
+    'labelright': 'off',
+    'top': 'off',
+    'labeltop': 'off'}
+
+ax1.tick_params(**tick_params)
+ax2.tick_params(**tick_params)
 
 # turn off all spines except lower x
-ax.spines['left'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
+ax1.spines['left'].set_visible(False)
+ax1.spines['right'].set_visible(False)
+ax1.spines['top'].set_visible(False)
+ax2.spines['left'].set_visible(False)
+ax2.spines['right'].set_visible(False)
+ax2.spines['top'].set_visible(False)
 
 # colour wheel
 colours = lf.Colours()
@@ -40,75 +48,65 @@ c3 = colours.next() # amplitude modulation sidebands
 # arrow arguments
 args = {'width': lf.ARROW_WIDTH, 'head_width': lf.ARROW_HEAD_WIDTH, 'head_length': lf.ARROW_HEAD_LENGTH, 'overhang': lf.ARROW_OVERHANG, 'length_includes_head': True}
 
-# amplitude modulation
-def am(E_0, omega_0, t, m, omega):
-  return [E_0 * np.exp(1j * omega_0 * t) * (m / 2) * np.exp(1j * omega * t), E_0 * np.exp(1j * omega_0 * t), E_0 * np.exp(1j * omega_0 * t) * (m / 2) * np.exp(-1j * omega * t)]
-
 # phase modulation
 def pm(E_0, omega_0, t, m, omega, n):
   
   # ith Bessel coefficient
   def ji(E_0, omega_0, t, m, omega, i):
-    return 1j ** i * np.i0(m) * np.exp(1j * i * omega * t)
+    return 1j ** i * special.jv(i, m) * np.exp(1j * i * omega * t)
   
   # return list of coefficients up to nth, multiplied by carrier
-  return E_0 * np.exp(1j * omega_0 * t) * [ji(E_0, omega_0, t, m, omega, ii) for ii in xrange(i)]
+  return E_0 * np.exp(1j * omega_0 * t) * [ji(E_0, omega_0, t, m, omega, ii) for ii in xrange(-i, i)]
 
-E_0 = 1
-c_0 = 3e8
-lambda_0 = 1064e-9
-omega_0 = 2 * np.pi * c_0 / lambda_0
-t = np.linspace(0, 1e-5, 100000)
-m = 0.1
-omega = 10e6
-
-am_fields = am(E_0, omega_0, t, m, omega)
-#[f, p_am] = scipy.signal.welch(sum(am_fields), fs=1/(t[1]-t[0]), window='hann', nperseg=10000)
-
-ax.plot(t, np.abs(am_fields[0]))
-#ax.semilogy(f, p_am)
-#ax.plot(t, am_fields[1])
-#ax.plot(t, am_fields[2], '--')
-#ax.set_xlim([0, 3e-14])
-plt.show()
-
-## plot carrier
-p1 = ax.arrow(0, 0, 0, 15, color=c1, **args)
+E_0 = 8
+m = 1
 
 ## plot amplitude modulation sidebands
-p2 = ax.arrow(-0.5, 0, 0, 8, color=c3, **args)
-ax.arrow(0.5, 0, 0, 8, color=c3, **args)
+# carrier
+p1 = ax1.arrow(0, 0, 0, E_0, color=c1, **args)
+
+# sidebands
+p2 = ax1.arrow(-0.5, 0, 0, E_0 * m / 2, color=c3, **args)
+ax1.arrow(0.5, 0, 0, E_0 * m / 2, color=c3, **args)
 
 ## plot phase modulation sidebands
-# first order
-p3 = ax.arrow(-0.5, 0, 0, 5, color=c2, alpha=0.8, **args)
-ax.arrow(0.5, 0, 0, 5, color=c2, alpha=0.8, **args)
+#carrier
+p3 = ax2.arrow(0, 0, 0, E_0 * special.jv(0, m), color=c1, **args)
+# first order sidebands
+p4 = ax2.arrow(-0.5, 0, 0, E_0 * special.jv(1, m), color=c2, **args)
+ax2.arrow(0.5, 0, 0, E_0 * special.jv(1, m), color=c2, **args)
 # second order
-ax.arrow(-0.6, 0, 0, 1.5, color=c2, alpha=0.8, **args)
-ax.arrow(-0.4, 0, 0, 1.5, color=c2, alpha=0.8, **args)
-ax.arrow(0.4, 0, 0, 1.5, color=c2, alpha=0.8, **args)
-ax.arrow(0.6, 0, 0, 1.5, color=c2, alpha=0.8, **args)
+ax2.arrow(-1, 0, 0, E_0 * special.jv(2, m), color=c2, **args)
+ax2.arrow(1, 0, 0, E_0 * special.jv(2, m), color=c2, **args)
 # third order
-ax.arrow(-0.65, 0, 0, 0.75, color=c2, alpha=0.8, **args)
-ax.arrow(-0.55, 0, 0, 0.75, color=c2, alpha=0.8, **args)
-ax.arrow(-0.45, 0, 0, 0.75, color=c2, alpha=0.8, **args)
-ax.arrow(-0.35, 0, 0, 0.75, color=c2, alpha=0.8, **args)
-ax.arrow(0.65, 0, 0, 0.75, color=c2, alpha=0.8, **args)
-ax.arrow(0.55, 0, 0, 0.75, color=c2, alpha=0.8, **args)
-ax.arrow(0.45, 0, 0, 0.75, color=c2, alpha=0.8, **args)
-ax.arrow(0.35, 0, 0, 0.75, color=c2, alpha=0.8, **args)
+ax2.arrow(-1.5, 0, 0, E_0 * special.jv(3, m), color=c2, **args)
+ax2.arrow(1.5, 0, 0, E_0 * special.jv(3, m), color=c2, **args)
 
-ax.legend([p1, p2, p3], ['Carrier', 'AM sidebands', 'PM sidebands'])
+ax1.legend([p1, p2], ['Carrier', 'AM sidebands'])
 
-ax.set_xticks([-0.5, 0, 0.5])
-ax.set_xticklabels([r'$\omega_0-\omega$', r'$\omega_0$', r'$\omega_0+\omega$'])
+# have to set ticks to be same as for ax2, but hide labels to
+# make it seem like they share x-axes (otherwise it would say
+# "Frequency" only on x-axis 2 but have labels on both)
+ax1.set_xticks([-1.5, -1, -0.5, 0, 0.5, 1, 1.5])
+ax1.set_xticklabels([])
 
-ax.set_xlabel('Frequency')
-ax.set_ylabel('Magnitude')
+ax2.legend([p3, p4], ['Carrier', 'PM sidebands'])
 
-ax.grid(True)
+ax2.set_xticks([-1.5, -1, -0.5, 0, 0.5, 1, 1.5])
+ax2.set_xticklabels([r'$\omega_0-3\omega$', r'$\omega_0-2\omega$', r'$\omega_0-\omega$', r'$\omega_0$', r'$\omega_0+\omega$', r'$\omega_0+2\omega$', r'$\omega_0+3\omega$'])
 
-ax.set_xlim([-1.3, 1.3])
-ax.set_ylim([0, 16])
+ax1.set_ylabel('Power (arb. units)')
+ax2.set_ylabel('Power (arb. units)')
+ax2.set_xlabel('Frequency')
 
-#plt.savefig(save_path)
+ax1.grid(True)
+ax2.grid(True)
+
+ax1.set_xlim([-1.8, 1.8])
+ax1.set_ylim([0, 8])
+ax2.set_xlim([-1.8, 1.8])
+ax2.set_ylim([0, 8])
+
+plt.tight_layout()
+
+plt.savefig(save_path)
